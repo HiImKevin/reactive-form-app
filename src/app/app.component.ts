969d911c7d9636330c3f8864f892fdfcf7725a25
HiImKevin <brandnew121@gmail.com>
2020-01-28 00:08:08 -0500

    adds reactive form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  constructor(private http: HttpClient) { }
+
   title = 'form-app';
+
+  disclosureForm = new FormGroup({
+  accountType: new FormControl(''),
+  fName: new FormControl(''),
+  mInitial: new FormControl(''),
+  lName: new FormControl(''),
+  email: new FormControl(''),
+  phoneNum: new FormControl(''),
+  formType: new FormControl('CRS'),
+  version: new FormControl('1.1')
+  });
+
+  onSubmit() {
+    console.log(this.disclosureForm.value);
+    this.http.post('/api/userAcknowledge', this.disclosureForm.value)
+      .subscribe((response)=>{
+        console.log('Response: ',response);
+      })
+  }
 }
